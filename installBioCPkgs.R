9d738b1007d1cf71aa27aad3da755a807f16cbc0
markdunning <mark.dunning@gmail.com>
2016-04-15 11:51:05 +0100

    update install script

--- installBioCPkgs.R
+++ installBioCPkgs.R
@@ -2,10 +2,7 @@ options(repos = c("CRAN" = "http://cran.ma.imperial.ac.uk"))
 install.packages(c("devtools","shiny"))
 library(devtools)
 source("http://www.bioconductor.org/biocLite.R")
-biocLite(c("Biobase","ggplot2","dplyr","tidyr","gridExtra","heatmap.plus","RColorBrewer","party","survival","knitr","broom","WGCNA","gplots"))
-install_github("crukci-bioinformatics/prostateCancerTaylor")
-install_github("crukci-bioinformatics/prostateCancerCamcap")
-install_github("crukci-bioinformatics/prostateCancerStockholm")
-install_github("crukci-bioinformatics/prostateCancerGrasso")
-install_github("crukci-bioinformatics/prostateCancerVarambally")
+biocLite(c("Biobase","ggplot2","dplyr","tidyr","gridExtra","heatmap.plus",
+           "RColorBrewer","party","survival","knitr","broom","WGCNA","gplots",
+           "prostateCancerTaylor","prostateCancerCamcap","prostateCancerStockholm","prostateCancerGrasso","prostateCancerVarambally","GGally"))
 
