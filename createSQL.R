bb68f9a388409df2f0d1a60c8c3f92fe381ed777
markdunning <mark.dunning@gmail.com>
2016-09-06 12:52:29 +0100

    remove duplication of varambally sql creation

--- createSQL.R
+++ createSQL.R
@@ -87,21 +87,6 @@ copy_to(db, expression, temporary = FALSE, indexes = list("Symbol"))
 copy_to(db, pd, temporary = FALSE, indexes = list("geo_accession"))
 copy_to(db, fd, temporary = FALSE, indexes = list("ID"))
 
-db <- src_sqlite("varambally.sqlite3",create=TRUE)
-
-data(varambally,package = "prostateCancerVarambally")
-pd_varambally <- tbl_df(pData(varambally))
-fd_varambally <- tbl_df(fData(varambally))
-expression <-
-  tbl_df(data.frame(ID = as.character(featureNames(varambally)),exprs(varambally))) %>% 
-  gather(geo_accession,Expression,-ID)
-fd <- fd_varambally
-pd <- pd_varambally
-expression <- left_join(expression, select(fd, ID, Symbol))
-
-copy_to(db, expression, temporary = FALSE, indexes = list("Symbol"))
-copy_to(db, pd, temporary = FALSE, indexes = list("geo_accession"))
-copy_to(db, fd, temporary = FALSE, indexes = list("ID"))
 
 db <- src_sqlite("grasso.sqlite3",create=TRUE)
 
