126d324bc52ab34360757bc2369e98a98fc0732f
markdunning <mark.dunning@gmail.com>
2017-01-18 14:17:20 +0000

    add missing comma in UI

--- UI.R
+++ UI.R
@@ -5,7 +5,7 @@ library(shiny)
 
 shinyUI(navbarPage("Explore Prostate Cancer Datasets", id = "nav",
                   
-    tags$head(includeScript("google-analytics.js"))               
+    tags$head(includeScript("google-analytics.js")),               
     #    tabPanel("MSKCC",
      #            sidebarLayout(
       #             sidebarPanel(
