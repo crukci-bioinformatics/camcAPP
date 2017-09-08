f1fe7805f02fc9257b4dc37a1b6abbe85ce30c58
markdunning <mark.dunning@gmail.com>
2017-09-08 12:56:13 +0100

    correct typo in dbplyr

--- installBioCPkgs.R
+++ installBioCPkgs.R
@@ -2,6 +2,6 @@ options(repos = c("CRAN" = "http://cran.ma.imperial.ac.uk"))
 install.packages(c("devtools","shiny"))
 library(devtools)
 source("http://www.bioconductor.org/biocLite.R")
-biocLite(c("org.Hs.eg.db","Biobase","ggplot2","dplyr","dbdplyr","tidyr","gridExtra","heatmap.plus",
+biocLite(c("org.Hs.eg.db","Biobase","ggplot2","dplyr","dbplyr","tidyr","gridExtra","heatmap.plus",
            "RColorBrewer","party","survival","knitr","broom","WGCNA","gplots",
            "prostateCancerTaylor","prostateCancerCamcap","prostateCancerStockholm","prostateCancerGrasso","prostateCancerVarambally","GGally","DT","ggthemes","stringr","COSMIC.67"))
